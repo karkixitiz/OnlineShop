4bedca46456a17fc35ded2196607c3a266748183
kiran.karki <kiran.karki@student.fh-kiel.de>
2020-05-09 23:29:30 +0200

    conflict resolve

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 ### Online Shop
     A simple Asp.Net Core 2.1 Online Shop Application using VueJs and bulma
 	
-	
+
+	## Learning
+	- VueJs
+	- Blume: is a free, open source CSS framework based on Flexbox. Download(bulma.io)
 	
 ### Note
 	- For Migration: add-migration name
--- Shop.UI/wwwroot/js/admin/main.js
+++ Shop.UI/wwwroot/js/admin/main.js
@@ -2,99 +2,111 @@
     el: '#app',
     data: {
         loading: false,
-        productModel: {
-            id: 0,
-            objectIndex: 0, 
-            name: "abc",
-            description: "efg",
-            value:12.2
+        editing: false,
+                productModel: {
+                    id: 0,
+                    objectIndex: 0,
+                    name: "abc",
+                    description: "efg",
+                    value: 12.2
+                },
+                products: []
         },
-        products: []
-    },
-    methods: {
-        getProduct(id) {
-            this.loading = true
-            axios.get('/Admin/products/'+id)
-                .then(res => {
-                    console.log(res);
-                    var product = res.data;
-                    this.productModel = {
-                        id: product.id,
-                        name: product.name,
-                        description: product.description,
-                        value: product.value
-                    }
-                })
-                .catch(err => {
-                    console.log(err)
-                })
-                .then(() => {
-                    this.loading = false
-                });
+        methods: {
+            getProduct(id) {
+                this.loading = true
+                axios.get('/Admin/products/' + id)
+                    .then(res => {
+                        console.log(res);
+                        var product = res.data;
+                        this.productModel = {
+                            id: product.id,
+                            name: product.name,
+                            description: product.description,
+                            value: product.value
+                        }
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    })
+                    .then(() => {
+                        this.loading = false
+                    });
+            },
+            getProducts() {
+                this.loading = true
+                axios.get('/Admin/products')
+                    .then(res => {
+                        console.log(res);
+                        this.products = res.data;
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    })
+                    .then(() => {
+                        this.loading = false
+                    });
+            },
+            createProduct() {
+                this.loading = true;
+                axios.post('/Admin/products', this.productModel)
+                    .then(res => {
+                        this.products.push(res.data);
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    })
+                    .then(() => {
+                        this.loading = false
+                        this.editing = false
+                    });
+            },
+            editProduct(id, index) {
+                this.objectIndex = index;
+                this.getProduct(id);
+                this.editing = true
+            },
+            updateProduct() {
+                this.loading = true;
+                axios.put('/Admin/products', this.productModel)
+                    .then(res => {
+                        this.products.splice(this.objectIndex, 1, res.data);
+                        this.productModel = ''
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    })
+                    .then(() => {
+                        this.loading = false
+                        this.editing = false
+                    });
+            },
+            deleteProduct(id, index) {
+                this.loading = true;
+                axios.delete('/Admin/products/' + id)
+                    .then(res => {
+                        this.products.splice(index, 1);
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    })
+                    .then(() => {
+                        this.loading = false
+                    });
+            },
+            cancel() {
+                this.editing = false;
+            },
+            newProduct() {
+                this.editing = true;
+                this.productModel.id = 0;
+            }
         },
-        getProducts(){
-            this.loading=true
-            axios.get('/Admin/products')
-                .then(res => {
-                    console.log(res);
-                    this.products = res.data;
-                })
-                .catch(err => {
-                    console.log(err)
-                })
-                .then(() => {
-                    this.loading = false
-                });
-        },
-        createProduct() {
-            this.loading = true;
-            axios.post('/Admin/products', this.productModel)
-                .then(res => {
-                    this.products.push(res.data);
-                })
-                .catch(err => {
-                    console.log(err)
-                })
-                .then(() => {
-                    this.loading = false
-                });
-        },
-        editProduct(id, index) {
-            this.objectIndex = index;
-            this.getProduct(id);
-        },
-        updateProduct() {
-            this.loading = true;
-            axios.put('/Admin/products', this.productModel)
-                .then(res => {
-                    this.products.splice(this.objectIndex, 1, res.data);
-                    this.productModel=''
-                })
-                .catch(err => {
-                    console.log(err)
-                })
-                .then(() => {
-                    this.loading = false
-                });
-        },
-        deleteProduct(id,index) {
-            this.loading = true;
-            axios.delete('/Admin/products/'+id)
-                .then(res => {
-                    this.products.splice(index, 1);
-                })
-                .catch(err => {
-                    console.log(err)
-                })
-                .then(() => {
-                    this.loading = false
-                });
-        },
-    },
-    computed: {
+        computed: {
 
-    },
-    mounted() {
-        this.getProducts();
-    }
-})
+        },
+        mounted() {
+            this.getProducts();
+        }
+    });
+   
