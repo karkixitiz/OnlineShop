b6811cf3946b36c8e2b764f5a03a4effe3634883
kiran.karki <kiran.karki@student.fh-kiel.de>
2020-05-12 09:12:30 +0200

    session

--- Shop.UI/Pages/Product.cshtml.cs
+++ Shop.UI/Pages/Product.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Shop.Application.Products;
@@ -18,9 +19,25 @@ namespace Shop.UI.Pages
             _ctx = ctx;
         }
         public ProductViewModel Product { get; set; }
-        public void OnGet(string name)
+        [BindProperty]
+        public Test ProductTest { get; set; }
+        public class Test
+        {
+            public string Id { get; set; }
+        }
+        public IActionResult OnGet(string name)
         {
             Product = new GetProduct(_ctx).Do(name.Replace("-"," "));
+            if (Product == null)
+                return RedirectToPage("Index");
+            else
+                return Page();
+        }
+        public IActionResult OnPost()
+        {
+            var current_id = HttpContext.Session.GetString("id");
+            HttpContext.Session.SetString("id", ProductTest.Id);
+            return RedirectToPage("Index");
         }
     }
 }
